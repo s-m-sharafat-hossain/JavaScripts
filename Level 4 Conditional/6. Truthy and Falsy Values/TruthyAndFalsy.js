304bcabec5b7ea7fe53ecad1cbd6804b2ff1167f
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2025-09-13 14:28:25 +0600

    fdgfg

---
+++ Level 4 Conditional/6. Truthy and Falsy Values/TruthyAndFalsy.js
@@ -0,0 +1,11 @@
+/*
+=> Truthy and Falsy Values:
+
+- Falsy Values: 0, null, undefined, false, NaN, ""(empty string)
+
+- Truthy Values: All values not listed as falsy.
+
+- Non-boolean values are auto-converted in logical operators.
+
+- Be explicit in comparisons to avoid unexpected behavior.
+*/
--- Level 4 Conditional/Project/CricketGame.js
+++ Level 4 Conditional/Project/CricketGame.js
@@ -1,5 +1,96 @@
 
 document.getElementById('b1').onclick= function(){
-    console.log('You Have chosen Bat')
     
+    const userChoice = 'You Have chosen Bat';
+
+    let randomNumber = Math.random()*3
+    let computerChoice;
+    if(randomNumber>0 && randomNumber<=1){
+        computerChoice = 'Bat';
+        console.log('Computer choice is Bat');
+    }else if(randomNumber>1 && randomNumber<=2){
+        computerChoice = 'Ball'
+        console.log('Computer Choice is Ball.')
+    }else{
+        computerChoice='Stump'
+        console.log('Computer Choice is Stump.');
+    }
+    const computerMassage= `Computer Choice ${computerChoice}`;
+    
+    //now match with user choice
+    let resultMassage;
+    if(computerChoice=== 'Ball'){
+        resultMassage= 'User Won'
+    }else if(computerChoice === 'Bat'){
+        resultMassage = 'Tie'
+    }else if(computerChoice === 'Stump'){
+        resultMassage = 'Computer won'
+    }
+
+    alert(`${userChoice}\n\n${computerMassage}\n\n${resultMassage}`);
+}
+
+//For Ball
+
+document.getElementById('b2').onclick= function(){
+    
+    const userChoice = 'You Have chosen Ball';
+
+    let randomNumber = Math.random()*3
+    let computerChoice;
+    if(randomNumber>0 && randomNumber<=1){
+        computerChoice = 'Bat';
+        console.log('Computer choice is Bat');
+    }else if(randomNumber>1 && randomNumber<=2){
+        computerChoice = 'Ball'
+        console.log('Computer Choice is Ball.')
+    }else{
+        computerChoice='Stump'
+        console.log('Computer Choice is Stump.');
+    }
+    const computerMassage= `Computer Choice ${computerChoice}`;
+    
+    //now match with user choice
+    let resultMassage;
+    if(computerChoice=== 'Bat'){
+        resultMassage= 'User Won'
+    }else if(computerChoice === 'Ball'){
+        resultMassage = 'Tie'
+    }else if(computerChoice === 'Stump'){
+        resultMassage = 'Computer won'
+    }
+
+    alert(`${userChoice}\n\n${computerMassage}\n\n${resultMassage}`);
+}
+//For Ball
+
+document.getElementById('b3').onclick= function(){
+    
+    const userChoice = 'You Have chosen Stump';
+
+    let randomNumber = Math.random()*3
+    let computerChoice;
+    if(randomNumber>0 && randomNumber<=1){
+        computerChoice = 'Bat';
+        console.log('Computer choice is Bat');
+    }else if(randomNumber>1 && randomNumber<=2){
+        computerChoice = 'Ball'
+        console.log('Computer Choice is Ball.')
+    }else{
+        computerChoice='Stump'
+        console.log('Computer Choice is Stump.');
+    }
+    const computerMassage= `Computer Choice ${computerChoice}`;
+    
+    //now match with user choice
+    let resultMassage;
+    if(computerChoice=== 'Ball'){
+        resultMassage= 'User Won'
+    }else if(computerChoice === 'Stump'){
+        resultMassage = 'Tie'
+    }else if(computerChoice === 'Bat'){
+        resultMassage = 'Computer won'
+    }
+
+    alert(`${userChoice}\n\n${computerMassage}\n\n${resultMassage}`);
 }
