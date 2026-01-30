859bc799e172a566cc11faf8baee3ccaf5149526
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2026-01-30 23:55:38 +0600

    Syntax

---
+++ 1. Basic JS/1. Introduction JS/3. Syntax/Syntax.js
@@ -0,0 +1,41 @@
+
+            //Write in the web page:
+
+document.write("<br>hi");
+// We can use HTML tag in document.write.
+document.write("<br>");
+document.write(45+45);
+document.write("<br>");
+
+//We can also use single quotation for print string.
+document.writeln('hi');
+document.writeln("<br>");
+//Writeln for new line.
+document.writeln(45+45);
+document.writeln("<br>");
+
+//We can use backtick(`) for string and expression.
+document.write(`hi ${45+45} <br>`);
+
+
+
+
+            //Write in the console:
+
+console.log("hi");
+console.log("<br>");
+console.log(45+45);
+console.log("<br>");
+
+//We can use backtick(`) for string and expression.
+console.log(`hi <b>${45+45}</b>`);
+console.log("<br>");
+
+
+
+            //Write in the alert box:
+            
+// alert("hi");
+// alert(45+45);
+
+
