a55292e9b0b20741075deba873f8bb9225e9436d
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2026-01-31 00:55:50 +0600

    Console

---
+++ 1. Basic JS/7. Browser Consol/Console.js
@@ -0,0 +1,11 @@
+
+//Browser Console:
+
+console.table([{name: "Hamim", age: 22, country: "Bangladesh"},{name: "Munim", age: 22, country: "Bangladesh"},{name: "Sharia", age: 22, country: "Bangladesh"}])
+
+
+console.time('Timer 1');
+for (let i = 0; i < 100000; i++) {
+    
+}
+console.timeEnd('Timer 1');
