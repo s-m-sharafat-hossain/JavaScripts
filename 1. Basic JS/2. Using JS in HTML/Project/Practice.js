4972e2ff873297348646f968ba320c23f7dc005d
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2026-01-31 00:17:03 +0600

    Practice

--- 1. Basic JS/2. Using JS in HTML/Project/Practice.js
+++ 1. Basic JS/2. Using JS in HTML/Project/Practice.js
@@ -1,6 +1,9 @@
 
-document.getElementById('b2').onclick = function() {
+document.getElementById('b1').onclick = function() {
     alert("Button clicked!");
 };
 
+document.getElementById('b4').onclick = function() {
+    window.open('https://www.google.com');
+};
 
