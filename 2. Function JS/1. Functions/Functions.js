7cc898ec4a6e4ad05fe270c4095c87219bee1f79
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2026-02-08 05:07:05 +0600

    function

---
+++ 2. Function JS/1. Functions/Functions.js
@@ -0,0 +1,11 @@
+/*
+=> Function is a block of code that performs a specific task.
+=> 
+=> Function is a first class citizen.
+*/
+
+function myFunction() {
+    console.log("Hello, world!");
+}
+
+myFunction();
