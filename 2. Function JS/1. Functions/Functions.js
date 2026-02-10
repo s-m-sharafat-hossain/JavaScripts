fca3f604f96faa1349387b960e8560e7388bc505
s-m-sharafat-hossain <sharafatcox60@gmail.com>
2026-02-10 06:45:16 +0600

    functions

--- 2. Function JS/1. Functions/Functions.js
+++ 2. Function JS/1. Functions/Functions.js
@@ -1,8 +1,17 @@
 /*
-=> Function is a block of code that performs a specific task.
-=> 
-=> Function is a first class citizen.
+=> Function is recusable block of code designed to perform a specific task.
+
+=> syntax: 
+    function functionName(parameters){
+        // function body   
+    }
+    functionName(parameters); //function call
+
 */
+const prompt = require("prompt-sync")();
+
+
+//function without parameters:
 
 function myFunction() {
     console.log("Hello, world!");
@@ -11,6 +20,34 @@ function myFunction() {
 myFunction();
 
 
-/*
-=> Function is a first class citizen.
-*/
+// Default parameters:
+
+function myFunction2(param1 = "default value") {
+    console.log(param1);
+}
+myFunction2(); // default value
+myFunction2("Hello"); // Hello
+
+
+// Function with parameters:
+
+var num1 = prompt("Enter first number: ");
+var num2 = prompt("Enter second number: ");
+
+function Sum(num1, num2) {
+    result = num1 + num2;
+    console.log(`Sum: ${result}`);
+}
+Sum(num1, num2);
+
+
+// Function with return value:
+
+function sub(num1, num2) {
+    result = num1 - num2;
+    return result;
+}
+console.log(`Subtraction: ${sub(num1, num2)}`);
+
+
+
